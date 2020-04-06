b898fd99d29d065b53e312ad38f485538531954c
Kit Yan <kit.yan@oocl.com>
2020-04-06 18:35:58 +0800

    Added printReceipt functions and context map

--- main/main.js
+++ main/main.js
@@ -1,5 +1,67 @@
 'use strict';
 
+function getItemDetail(barcode, allItemsList) {
+    return allItemsList.find(item => item.barcode == barcode);
+}
+
+function decodeItemList(inputs) {
+    const allItemsList = loadAllItems();
+    return inputs.reduce(function (orderedItems, barcode) {
+        let itemDetail = getItemDetail(barcode, allItemsList);
+        let data = orderedItems.find(item => item.barcode == barcode);
+        if (data) {
+            data.quantity++;
+            data.unit = (data.quantity == 2) ? data.unit + "s" : data.unit;
+        }
+        else
+            orderedItems.push({
+                barcode: itemDetail.barcode,
+                name: itemDetail.name,
+                unit: itemDetail.unit,
+                unitPrice: itemDetail.price,
+                quantity: 1
+            });
+        return orderedItems;
+    }, []);
+}
+
+function calculateSubTotalPrice(detailItemList) {
+    return detailItemList.map(detailItem => {
+        return {
+            barcode: detailItem.barcode, name: detailItem.name, unit: detailItem.unit,
+            unitPrice: detailItem.unitPrice, quantity: detailItem.quantity,
+            subTotalPrice: detailItem.unitPrice * detailItem.quantity
+        }
+    });
+}
+function calculateTotalPrice(itemSubSummaryList) {
+    let totalPrice = 0;
+    itemSubSummaryList.forEach(detailItem => totalPrice += detailItem.subTotalPrice);
+    return totalPrice;
+}
+
+function generateItemSummary(detailItemList) {
+    let itemSubSummaryList = calculateSubTotalPrice(detailItemList);
+    return { itemSubSummaryList: itemSubSummaryList, totalPrice: calculateTotalPrice(itemSubSummaryList) };
+}
+
+function formatItemSummary(itemSummary) {
+    return "Name：" + itemSummary.name + "，Quantity：" + itemSummary.quantity + " " + itemSummary.unit
+        + "，Unit：" + itemSummary.unitPrice.toFixed(2) + " (yuan)，Subtotal：" + itemSummary.subTotalPrice.toFixed(2)
+        + " (yuan)\n";
+}
+
+function formatReceipt(itemSummaryList) {
+    let receiptString = "***<store earning no money>Receipt ***\n";
+    itemSummaryList.itemSubSummaryList.forEach(itemSummary => {
+        return receiptString += formatItemSummary(itemSummary);
+    })
+    receiptString += "----------------------\nTotal：" + itemSummaryList.totalPrice.toFixed(2) + " (yuan)\n**********************";
+    return receiptString;
+}
+
 function printReceipt(inputs) {
-  console.log('Implement the exercise requirements here and rewrite the line of code.');
+    let detailItemList = decodeItemList(inputs);
+    let itemSummaryList = generateItemSummary(detailItemList)
+    console.log(formatReceipt(itemSummaryList));
 }
